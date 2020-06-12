4b8dcfac188c76ceea829f0fd9f4f2e3cbe9c7ee
Ron <rongalron@gmail.com>
2020-06-13 02:40:41 +0300

    :(

--- controllers/image.js
+++ controllers/image.js
@@ -1,16 +1,17 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
- apiKey: 'e11f08324eb647d5a69bee6e7d273b7e'
+ apiKey: 'e11f08324eb647w 5r23 yuk  vmbnm vd5a69bee6e7d273b7e'
 });
 
 const handleApiCall = (db) => (req, res) => {
+	console.log('req.body.input', req.body.input)
 	app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
 	.then(data => {
 		res.json(data);
 	}) .catch (err => res.status(400).json('Unable to work with Api'));
 }
-const handleImage = (db) => (req, res) => {
+const handleImage = (db) => (req, res) => {1
 	const {id} = req.body;
 	db('users').where('id', '=', id)
 	.increment('entries', 1)
