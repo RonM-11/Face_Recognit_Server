c7503a5b4d98e2641ee04d218a18d791e5d7f5ce
Ron <rongalron@gmail.com>
2020-06-13 04:01:42 +0300

    :(((((((((((

--- controllers/image.js
+++ controllers/image.js
@@ -6,7 +6,7 @@ const app = new Clarifai.App({
 
 const handleApiCall = (db) => (req, res) => {
 	console.log('req.body.input', req.body.input)
-	app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+	app.models.predict(Clarifai.FACE_DETECTION_MODEL, req.body.input)
 	.then(data => {
 		res.json(data);
 	}) .catch (err => res.status(400).json('Unable to work with Api'));
